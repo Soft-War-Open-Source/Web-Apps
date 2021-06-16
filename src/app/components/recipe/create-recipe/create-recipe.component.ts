b18b414aca0d19b8345d83c582c109d558c662c5
Vetaman999 <68401851+Vetaman999@users.noreply.github.com>
2021-06-16 17:33:06 -0500

    create - list recipe
    create list en html

--- src/app/components/recipe/create-recipe/create-recipe.component.ts
+++ src/app/components/recipe/create-recipe/create-recipe.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Nutritionist } from 'src/app/model/nutritionist';
+import { Recipe } from 'src/app/model/recipe';
+import { RecipeService } from 'src/app/services/recipe.service';
+import { NutritionistService } from 'src/app/services/nutritionist.service';
 
 @Component({
   selector: 'app-create-recipe',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateRecipeComponent implements OnInit {
 
-  constructor() { }
+  recipe: Recipe = new Recipe();
+  nutritionist: Nutritionist=new Nutritionist();
+  nutritionist_id: number = 1;
+
+  constructor(private route: ActivatedRoute,private router: Router,private recipeService : RecipeService,private nutritionistService : NutritionistService) { }
 
   ngOnInit(): void {
+  }
+
+  assignNutritionist(id:number){
+    this.nutritionistService.getNutritionistById(id).subscribe(datos=>this.nutritionist=datos,error=>console.log(error));
+    console.log(this.nutritionist);
+  }
+  
+
+  insertRecipe(){
+    this.assignNutritionist(this.nutritionist_id);
+    this.recipe.nutritionist = this.nutritionist;
+    this.recipeService.createRecipe(this.recipe).subscribe(datos=>console.log(datos),error=>console.log(error));
+    this.recipe = new Recipe();
+    this.router.navigate(['newrecipe']);
   }
 
 }
--- src/app/components/recipe/list-recipe/list-recipe.component.ts
+++ src/app/components/recipe/list-recipe/list-recipe.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeService } from 'src/app/services/recipe.service';
+import { Router } from '@angular/router';
+import { Recipe } from 'src/app/model/recipe';
 
 @Component({
   selector: 'app-list-recipe',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListRecipeComponent implements OnInit {
 
-  constructor() { }
+  name: string='';
+  recipes: Recipe[]=[];
+
+
+  constructor(private router: Router, private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.loadDataRecipes();
+  }
+
+  loadDataRecipes(){
+    this.recipeService.getRecipeList().subscribe(recipes=>this.recipes=recipes);
+  }
+
+  deleteRecipe(recipe: Recipe){
+    this.recipeService.deleteRecipe(recipe.id).subscribe(data=>{this.loadDataRecipes();})
+  }
+
+  updateRecipe(recipe:Recipe){
+    this.router.navigate(['actualizar',recipe.id]);
+  }
+
+  searchRecipeByName(){
+    if(this.name.length!=0){
+      this.recipeService.getRecipeByName(this.name).subscribe(recipes=>this.recipes=recipes);
+    } 
+    else{
+      this.loadDataRecipes();
+    }
   }
 
 }
