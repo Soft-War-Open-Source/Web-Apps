be91dbf58a706b24b7c9b21fe370261d69653373
elmoebuta <u201726329@upc.edu.pe>
2021-06-16 20:04:19 -0500

    recommendation parte 2

--- src/app/services/recommendation.service.ts
+++ src/app/services/recommendation.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecommendationService {
 
-  constructor() { }
+  private baseURL= 'http://localhost:8080/api/recommendations';
+
+  constructor(private http: HttpClient) { }
+
+  createRecommendation(recommendation: Object): Observable<Object>{
+    return this.http.post(`${this.baseURL}`, recommendation);
+  }
+
+  updateRecommendation(id: number, recommendation: Object) : Observable<any>{
+    return this.http.put(`${this.baseURL}/${id}`, recommendation);
+  }
+
+  deleteRecommendation(id: number): Observable<any>{
+    return this.http.delete(`${this.baseURL}/${id}`);
+  }
+
+  getRecommendationList(): Observable<any>{
+    return this.http.get(`${this.baseURL}`);
+  }
+
+  getRecommendationById(id: number) : Observable<any>{
+    return this.http.get(`${this.baseURL}/${id}`)
+  }
+
+  getRecommendationByName(lastName: string) : Observable<any>{
+    return this.http.get(`${this.baseURL}/searchByName/${name}`)
+  }
 }
