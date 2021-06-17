d85c09abea3b74887cb3b22d0c64f84e4ffed2c8
zloop1995 <zloop1995@gmail.com>
2021-06-17 13:50:05 -0500

    fixes
    fixes

--- src/app/components/available-nutritionists/available-nutritionists.component.ts
+++ src/app/components/available-nutritionists/available-nutritionists.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Nutritionist } from 'src/app/model/nutritionist';
+import { NutritionistService } from 'src/app/services/nutritionist.service';
 
 @Component({
   selector: 'app-available-nutritionists',
@@ -8,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 export class AvailableNutritionistsComponent implements OnInit {
 
   cards:number[]=[1,2,3,4,5,6,7,8,9,10];
-  constructor() { }
+  nutritionists: Nutritionist[]=[];
+
+  constructor(private router: Router,
+  private nutritionistService: NutritionistService) { }
 
   ngOnInit(): void {
+    this.loadDataAppointments();
   }
 
+  loadDataAppointments(){
+    this.nutritionistService.getNutritionistList()
+    .subscribe(nutritionists=>this.nutritionists=nutritionists);
+  }
 }
