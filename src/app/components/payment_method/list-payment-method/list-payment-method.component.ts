8fa6478ddfa06db3798e49aa3585bda4f88d9a80
Josue <josue.c1012@gmail.com>
2021-06-16 20:28:59 -0500

    payment-method

--- src/app/components/payment_method/list-payment-method/list-payment-method.component.ts
+++ src/app/components/payment_method/list-payment-method/list-payment-method.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PaymentMethodService } from 'src/app/services/payment-method.service';
+import { PaymentMethod } from 'src/app/model/payment_method';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-payment-method',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-payment-method.component.css']
 })
 export class ListPaymentMethodComponent implements OnInit {
+  id: number = 0;
+  client_id: number = 0;
+  paymentMethods: PaymentMethod[] = [];
 
-  constructor() { }
+  constructor(private router: Router, private paymentMethodService: PaymentMethodService) { }
 
   ngOnInit(): void {
+    this.loadDataPaymentMethods();
+  }
+
+  loadDataPaymentMethods(){
+    this.paymentMethodService.getPaymentMethodList()
+    .subscribe(paymentsMethods=>this.paymentMethods = this.paymentMethods);
+  }
+
+  deletePaymentMethod(payment_method: PaymentMethod){
+    this.paymentMethodService.deletePaymentMethod(payment_method.id)
+    .subscribe(data=>{this.loadDataPaymentMethods();})
+  }
+
+  updatePaymentMethod(payment_method: PaymentMethod){
+    this.router.navigate(['actualizar', payment_method.id]);
+  }
+
+  searchPaymentMethodById(){
+    if(this.id <= 0){
+      this.paymentMethodService.getPaymentMethodById(this.id)
+      .subscribe(paymentMethods=>this.paymentMethods = paymentMethods);
+    }
+  }
+
+  searchPaymentMethodByClient(){
+    if(this.client_id <= 0){
+      this.paymentMethodService.getPaymentMethodByClient(this.client_id)
+      .subscribe(paymentMethods=>this.paymentMethods = paymentMethods);
+    }
   }
 
 }
