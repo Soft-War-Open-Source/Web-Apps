b17361ee04b5a0e62c5d81df6f43ca9c1a5f32f9
Josue <josue.c1012@gmail.com>
2021-06-17 16:55:21 -0500

    bill implementacion

--- src/app/components/bill/create-bill/create-bill.component.ts
+++ src/app/components/bill/create-bill/create-bill.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Client } from 'src/app/model/client';
+import { Bill } from 'src/app/model/bill';
+import { ClientService } from 'src/app/services/client.service';
+import { BillService } from 'src/app/services/bill.service';
 
 @Component({
   selector: 'app-create-bill',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateBillComponent implements OnInit {
 
-  constructor() { }
+  client_id: number = 1;
+  bills: Bill[]=[];
+  clients: Client[]=[];
 
+  constructor(private router: Router,
+    private billService: BillService,
+    private clientService : ClientService) { }
+
+  
   ngOnInit(): void {
   }
 
+  assignClient(){
+    this.clientService.getClientById(this.client_id)
+    .subscribe(datos=>this.clients.push(datos));
+  }
+
+  insertBill(){
+    this.bills[0].client = this.clients[0];
+    this.billService.createBill(this.bills[0])
+    .subscribe(datos=>console.log(datos), error=>console.log(error));
+    this.bills = [];
+    this.clients = [];
+    this.router.navigate(['list-bills']);
+  }
 }
--- src/app/components/bill/list-bill/list-bill.component.ts
+++ src/app/components/bill/list-bill/list-bill.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { BillService } from 'src/app/services/bill.service';
+import { Bill } from 'src/app/model/bill';
+import { Router } from '@angular/router';
+import { Client } from 'src/app/model/client';
+import { ClientService } from 'src/app/services/client.service';
 
 @Component({
   selector: 'app-list-bill',
@@ -7,9 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListBillComponent implements OnInit {
 
-  constructor() { }
+  client_id: number = 1;
+  clients: Client[]=[]
+  bills: Bill[]=[];
+  bills2: Bill[]=[];
+
+  constructor(private router: Router,
+    private billService: BillService,
+    private clientService: ClientService) { }
 
   ngOnInit(): void {
+    this.bills.push(new Bill());
+    this.assignClient();
+    this.loadDataBills();
   }
 
+  loadDataBills(){
+    this.billService.getBillByClient(this.client_id)
+    .subscribe(bills2=>this.bills2 = bills2);
+  }
+
+  deleteBill(bill: Bill){
+    this.billService.deleteBill(bill.id)
+    .subscribe(data=>{this.loadDataBills();})
+  }
+
+  updateBill(bill: Bill){
+    this.router.navigate(['actualizar', bill.id]); //cambiar enlace update
+  }
+
+  assignClient(){
+    this.clientService.getClientById(this.client_id)
+    .subscribe(datos=>this.clients.push(datos));
+  }
+
+  insertBill(){
+    this.bills[0].client = this.clients[0];
+    this.billService.createBill(this.bills[0])
+    .subscribe(datos=>console.log(datos), error=>console.log(error));
+    this.bills = [];
+    this.clients = [];
+    this.router.navigate(['list-payment-methods']);
+  }
 }
