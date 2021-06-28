686ed1f8858529192e0617b575040b11bed70204
zloop1995 <zloop1995@gmail.com>
2021-06-28 16:37:10 -0500

    Hot fixes
    hot fixes

--- src/app/components/recipe/list-recipes-nutritionist/list-recipes-nutritionist.component.ts
+++ src/app/components/recipe/list-recipes-nutritionist/list-recipes-nutritionist.component.ts
@@ -1,4 +1,8 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Recipe } from 'src/app/model/recipe';
+import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
   selector: 'app-list-recipes-nutritionist',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListRecipesNutritionistComponent implements OnInit {
 
-  constructor() { }
+  date: Date = new Date();
+  name: string = "";
+  date2: string = "";
+  client_id: number = 0;
+  recipes: Recipe[]=[];
+
+  constructor(private router: Router,
+    private route: ActivatedRoute,
+    private recipeService: RecipeService,
+    private datepipe: DatePipe) { }
 
   ngOnInit(): void {
+    this.client_id = this.route.snapshot.params['id'];
+    this.recipeService.getRecipeList()
+    .subscribe(datos=>{
+      console.log(datos)
+      this.recipes=datos;
+    }, error=>console.log(error));
+    
+    let today_date = this.datepipe.transform(this.date, 'yyyy-MM-dd');
+    if (today_date != null)
+    {
+      this.date2 = today_date;
+    }
   }
 
 }
